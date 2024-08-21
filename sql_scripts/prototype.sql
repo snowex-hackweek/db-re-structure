725e873505e7ba90351f4ec95b08b0614c1bb2ea
aaarendt <arendta@uw.edu>
2024-08-21 11:11:00 -0700

    change from insert to update

--- sql_scripts/prototype.sql
+++ sql_scripts/prototype.sql
@@ -9,26 +9,27 @@ alter table instruments
 
 select * from instruments
 
+-- run only to recreate the points_prototype table:
+-- create table points_prototype as select * from points_original;
+
+-- create the foreign key
 alter table points_prototype 
   add instrument_id INT,
   add constraint fk_instrument
   foreign key (instrument_id)
   references instruments (id);
-  
-insert into points_prototype (instrument_id)
-  select instruments.id
-  from instruments 
-  join points_prototype on
-  instruments.name = points_prototype.instrument;
+
+-- populate with indices from the instruments table
+update points_prototype
+set instrument_id = instruments.id from instruments
+where points_prototype.instrument = instruments.name;
  
+ -- create index for faster searching
  CREATE INDEX instrument_id_idx ON points_prototype (instrument_id);
  
-explain select * from points_prototype as p, instruments as i
-where i.name ='pulse EKKO Pro multi-polarization 1 GHz GPR';
-
-select * from points_prototype as p left join instruments as inst
+explain select * from points_prototype as p left join instruments as inst
 on p.instrument_id = inst.id
 where inst.name ='pulse EKKO Pro multi-polarization 1 GHz GPR';
 
-select * from points_prototype
+explain select * from points_prototype
 where instrument = 'pulse EKKO Pro multi-polarization 1 GHz GPR';
